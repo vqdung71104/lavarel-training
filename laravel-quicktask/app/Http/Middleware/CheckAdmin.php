40e5018d680ef15c5c07715910a56d8d705d721a
Vu Quang Dung <vuquangdung71104@gmail.com>
2025-07-30 16:46:15 +0700

    middleware

---
+++ laravel-quicktask/app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+        if (!$user || !$user->isAdmin()) {
+            return $next($request);
+        }
+        abort(403, 'Unauthorized action.');
+    }
+}
--- laravel-quicktask/routes/web.php
+++ laravel-quicktask/routes/web.php
@@ -5,12 +5,15 @@ use App\Http\Controllers\UserController;
 use App\Models\User;
 use App\Http\Controllers\TaskController;
 
+Auth::loginUsingId(1); // Automatically log in the first user for testing purposes
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/users', [UserController::class,'index'])->name('users.index');
-Route::get('/users/create', [UserController::class,'create'])->name('users.create');
+Route::get('/users/create', [UserController::class,'create'])
+->name('users.create')->middleware('admin');
 Route::get('/users/edit', [UserController::class,'edit'])->name('users.edit');
 Route::post('/users/store', [UserController::class,'store'])->name('users.store');
 Route::get('/users/{user}', [UserController::class,'show'])->name('users.show');
